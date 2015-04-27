c0c8d73345391da2a871d9f43dc5b7b98cca0eb0
zing321 <greeneagle692@aol.com>
2015-04-27 02:29:31 -0500

    fixed UpdateItemTable

--- cloud/commands/database/UpdateItemTable.js
+++ cloud/commands/database/UpdateItemTable.js
@@ -1,44 +1,41 @@
 'use strict';
 var _ = require('underscore');
-var SMITE_API=require('cloud/SmiteAPI.js');
+var SMITE_API = require('cloud/SmiteAPI.js');
 
-var setServerItems = function(items){
-  var saveArray=[];
-  _.each(items, function(item){
-    var itemsObject=new Parse.Object("Item");
-    itemsObject.set("Root", //obj RootItemId);
-    itemsObject.set("Parent", //obj ChildItemId);
-    itemsObject.set("Name",item.DeviceName);
-    itemsObject.set("Description",item.ItemDescription.Description);
-    itemsObject.set("SecondaryDescription", item.ItemDescription.SecondaryDescription);
-    itemsObject.set("ItemId",item.ItemId);
-    itemsObject.set("ItemTier",item.ItemTier);
-    itemsObject.set("Price",item.Price);
-    itemsObject.set("RootItemId",item.RootItemId);
-    itemsObject.set("ShortDesc",item.ShortDesc);
+var setServerItems = function(items) {
+  var saveArray = [];
+  _.each(items, function(item) {
+    var itemsObject = new Parse.Object('Item');
+    itemsObject.set('Name', item.DeviceName);
+    itemsObject.set('Description', item.ItemDescription.Description);
+    itemsObject.set('SecondaryDescription', item.ItemDescription.SecondaryDescription);
+    itemsObject.set('ItemId', item.ItemId);
+    itemsObject.set('Price', item.Price);
+    itemsObject.set('ShortDesc', item.ShortDesc);
+    itemsObject.set('Type', item.Type);
 
-    _.each(item.ItemDescription.Menuitems, function(menuItem){
-      itemsObject.add("Traits",menuItem);
+    _.each(item.ItemDescription.Menuitems, function(menuItem) {
+      itemsObject.add('Traits', menuItem);
     });
     saveArray.push(itemsObject);
   });
   return Parse.Object.saveAll(saveArray);
 };
 
-exports.updateTable=function(){
+exports.updateTable = function() {
 
-  return SMITE_API.getItems().then(function(items){
+  return SMITE_API.getItems().then(function(items) {
     return setServerItems(items);
   });
 };
 
-Parse.Cloud.job("updateItemTable", function(request, status){
-  exports.updateTable().then(function(){
-    console.log("Updated item table");
-    status.success("Updated item table");
+Parse.Cloud.job('updateItemTable', function(request, status) {
+  exports.updateTable().then(function() {
+    console.log('Updated item table');
+    status.success('Updated item table');
   },
-  function(error){
-    console.error("Error updating item table: " + error.message);
-    status.error("Error updating item table: " + error.message);
+  function(error) {
+    console.error('Error updating item table: ' + error.message);
+    status.error('Error updating item table: ' + error.message);
   });
 });
